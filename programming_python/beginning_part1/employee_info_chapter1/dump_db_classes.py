6c42084c55d3bb80a82f48c195915272fb57accf
weiwang <weiwang@redhat.com>
2017-09-04 21:38:49 +0800

    update

---
+++ programming_python/beginning_part1/employee_info_chapter1/dump_db_classes.py
@@ -0,0 +1,11 @@
+#dump_db_classes.py
+
+import shelve
+db = shelve.open('class-shelve')
+for key in db:
+	print(key, '=>\n', db[key].name, db[key].pay)
+
+bob = db['bob']
+print(bob.lastName())
+print(db['tom'].lastName())
+db.close()
