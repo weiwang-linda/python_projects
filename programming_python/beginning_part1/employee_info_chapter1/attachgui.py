966c2d8253850ae8aaa1ba0dedd1d2a1b9a75cd3
weiwang <weiwang@redhat.com>
2017-09-04 22:56:49 +0800

    some examples for tkinter usage

---
+++ programming_python/beginning_part1/employee_info_chapter1/attachgui.py
@@ -0,0 +1,12 @@
+#attachgui.py
+
+from tkinter import *
+from tkinter102 import MyGui
+
+mainwin = Tk()
+Label(mainwin, text=__name__).pack()
+
+popup = Toplevel()
+Label(popup, text='Attach').pack(side=LEFT)
+MyGui(popup).pack(side=RIGHT)
+mainwin.mainloop()
---
+++ programming_python/beginning_part1/employee_info_chapter1/tkinter001.py
@@ -0,0 +1,5 @@
+#tkinter001.py
+
+from tkinter import *
+Label(text="Spam").pack()
+mainloop()
