8ee10a1d223cd174a9369d771d013e75a7b4e70a
weiwang <weiwang@redhat.com>
2017-09-08 09:57:29 +0800

    some test about urlopen

---
+++ offical_website_exercises/internet_19_21/urllib/urllib_request_urlopen.py
@@ -0,0 +1,63 @@
+#Do some practice with urlopen method of urllib.request module.
+
+# para:    "url" : a string or Request object
+# para:    others : have default value
+
+# HTTP/1.1 and includes Connection:close header in its HTTP requests.
+
+# Always returns a "http.client.HTTPResponse" object which can work as a context manager and has methods such as:
+# >>> dir(html)
+#['begin', 'chunk_left', 'chunked', 'close', 'closed', 'code', 'debuglevel', 'fileno', 'flush', 
+#'fp', 'getcode', 'getheader', 'getheaders', 'geturl', 'headers', 'info', 'isatty', 'isclosed', 
+#'length', 'msg', 'read', 'readable', 'readall', 'readinto', 'readline', 'readlines', 'reason', 
+#'seek', 'seekable', 'status', 'tell', 'truncate', 'url', 'version', 'will_close', 'writable', 
+#'writelines']
+
+# Raises URLError on protocol errors.
+
+from urllib.request import urlopen
+import pprint
+
+URLS = ('https://github.com',)
+
+def displayHeaders(url):
+	http_response = urlopen(url)
+	if not http_response:
+		print("No handler handle this request!")
+	else:
+		print(http_response.geturl())
+		pprint.pprint(dict(http_response.getheaders()))
+
+def urlrequest_success(url):
+	http_response = urlopen(url)
+	print(http_response.__class__.__name__)
+	if http_response.msg == 'OK' and http_response.getcode() == 200:
+		print("The url is requested successfully!")
+	else:
+		print("The url is requested fail!")
+
+def get_htmltype(url):
+	try:
+		http_response = urlopen(url)
+		content_type = http_response.getheader('Content-Type').split(';')
+		print(content_type)
+	except URLError as e:
+		print(e)
+
+def search_sourcecode(url):
+	try:
+		http_response = urlopen(url)
+	except URLError as e:
+		print(e)
+	with http_response as response:
+		for line in response:
+			line = line.decode('utf-8')
+			if 'Sign up for GitHub' in line:
+				print(line)
+
+if __name__ == '__main__':
+	for url in URLS:
+		displayHeaders(url)
+		urlrequest_success(url)
+		get_htmltype(url)
+		search_sourcecode(url)
